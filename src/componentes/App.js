671751c0928e4ea2e190d465c9edf291028211c0
J3duardo <jesus_69eduardo@hotmail.com>
2019-04-26 15:19:02 -0400

    Agregado el header al componente App

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -5,11 +5,13 @@ import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <BrowserRouter>
           <div>
             <Route path="/" exact component={StreamList}/>
