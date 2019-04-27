7ba7b96ff770f9f6f7116972742831cd9c462e7b
J3duardo <jesus_69eduardo@hotmail.com>
2019-04-27 10:52:56 -0400

    Inicializado el cliente de la api de google en el componente GoogleAuth

--- src/componentes/GoogleAuth.js
+++ src/componentes/GoogleAuth.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
+  state = {
+    isSignedIn: null
+  }
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
       window.gapi.client.init({
         clientId: "542633267573-im2v8ed5ejgastcooclg0i7srs9s4ios.apps.googleusercontent.com",
         scope: "email"
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({
+          isSignedIn: this.auth.isSignedIn.get()
+        })
       })
     })
   }
+  
   render() {
     return (
       <div>
