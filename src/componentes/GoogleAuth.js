31c1461904c9af75a2633df775e516afc54d3d40
J3duardo <jesus_69eduardo@hotmail.com>
2019-04-27 11:28:02 -0400

    Agregado botón para hacer login y logout con google

--- src/componentes/GoogleAuth.js
+++ src/componentes/GoogleAuth.js
@@ -26,10 +26,30 @@ class GoogleAuth extends Component {
     })
   }
 
+  renderAuthButton = () => {
+    if (this.state.isSignedIn === null) {
+      return null;
+    }else if (this.state.isSignedIn) {
+      return (
+        <button className="ui red google button">
+          <i className="google icon"/>
+          Sign Out
+        </button>
+      )
+    }else {
+      return (
+        <button className="ui red google button">
+          <i className="google icon"/>
+          Sign In with Google
+        </button>
+      )
+    }
+  }
+
   render() {
     return (
       <div>
-        Google Auth
+        {this.renderAuthButton()}
       </div>
     );
   }
