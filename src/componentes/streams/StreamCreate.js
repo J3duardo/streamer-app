3e217df0df9335b7b0b2a9d361d9586c28a66057
J3duardo <jesus_69eduardo@hotmail.com>
2019-04-29 08:35:45 -0400

    Implementado redux-form en el componente StreamCreate para manejar la data del formulario

--- src/componentes/streams/StreamCreate.js
+++ src/componentes/streams/StreamCreate.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Field, reduxForm} from "redux-form";
 
-const StreamCreate = () => {
-  return (
-    <div>
-      Stream Create
-    </div>
-  );
+class StreamCreate extends Component {
+  renderInput = (formProps) => {
+    return (
+      <div className="field">
+        <label>{formProps.label}</label>
+        <input
+          type="text"
+          placeholder={formProps.placeholder}
+          {...formProps.input}
+        />      
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} placeholder="Title..." label="Enter Title:"/>
+        <Field name="description" component={this.renderInput} placeholder="Description" label="Enter Description:"/>
+      </form>
+    );
+  }
 }
 
-export default StreamCreate;
+export default reduxForm({
+  form: "StreamCreate"
+})(StreamCreate);
+
