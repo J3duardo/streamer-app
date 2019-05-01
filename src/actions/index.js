ce26e8cb9f5430b9a6d1343a5059d890decc4aa3
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-01 11:34:28 -0400

    Creado el action creator para crear nuevos streams

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import streams from "../axiosInstance/streams";
+
 export const signIn = (userId) => {
   return {
     type: "SIGN_IN",
@@ -8,5 +10,17 @@ export const signIn = (userId) => {
 export const signOut = () => {
   return {
     type: "SIGN_OUT"
+  }
+}
+
+export const createStream = (formValues) => {
+  const request = streams.post("/streams", formValues)
+  .then(response => {
+    return response.data
+  });
+
+  return {
+    type: "CREATE_POSTS",
+    payload: request
   }
 }
