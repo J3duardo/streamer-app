eb1824118b7808686345d25ff268d3a392bd4c45
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-04 09:44:40 -0400

    Refactorizado el componente StreamCreate para manejar el formulario desde StreamForm

--- src/componentes/streams/StreamCreate.js
+++ src/componentes/streams/StreamCreate.js
@@ -1,82 +1,22 @@
 import React, { Component } from 'react';
-import {Field, reduxForm} from "redux-form";
 import {connect} from "react-redux";
 import {createStream} from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamCreate extends Component {
-  renderInput = (formProps) => {
-    const className = `field ${formProps.meta.touched && formProps.meta.error ? "error" : ""}`;
-    return (
-      <div className={className}>
-        <label>{formProps.label}</label>
-        <input
-          type="text"
-          placeholder={formProps.placeholder}
-          autoComplete="off"
-          {...formProps.input}
-        />
-        {this.renderError(formProps.meta)}
-      </div>
-    )
-  }
-
-  renderError = (meta) => {
-    if(meta.touched && meta.error) {
-      return (
-        <div className="ui error message" style={{display: "block", fontSize: "12px"}}>
-          <div className="header">{meta.error}</div>
-        </div>
-      )
-    }else {
-      return null
-    }
-  }
-
-  onSubmitHandler = (formValues) => {
-    this.props.createNewStream(formValues, this.props.userId);
+  onSubmit = (formValues, userId) => {
+    this.props.createNewStream(formValues, userId);
   }
 
   render() {
-    if(this.props.userId) {
-      return (
-        <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmitHandler)}>
-          <Field name="title" component={this.renderInput} placeholder="Title..." label="Enter Title:"/>
-          <Field name="description" component={this.renderInput} placeholder="Description" label="Enter Description:"/>
-          <button className="ui button primary">Submit</button>
-        </form>
-      );
-    } else {
-      return (
-        <h2>You must be logged-in in order to create streams</h2>
-      )
-    }
+    return (
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
+    )
   }
 }
-
-const validateForm = (formValues) => {
-  const errors = {};
-
-  if(!formValues.title) {
-    errors.title = "You must enter a title"
-  }
-
-  if(!formValues.description) {
-    errors.description = "You must enter a description"
-  }
-
-  return errors;
-}
-
-const mapStateToProps = (state) => {
-  return {
-    userId: state.auth.userId
-  }
-}
-
-const form = reduxForm({
-  form: "StreamCreate",
-  validate: validateForm
-})(StreamCreate);
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -86,5 +26,5 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(form)
+export default connect(null, mapDispatchToProps)(StreamCreate)
 
