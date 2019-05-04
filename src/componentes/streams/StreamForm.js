9e6f01878c5299978f7093de6cf8887c17406468
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-04 09:31:06 -0400

    Creado el componente StreamForm para manejar los formularios sin repetición de código

---
+++ src/componentes/streams/StreamForm.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import {Field, reduxForm} from "redux-form";
+import {connect} from "react-redux";
+
+class StreamForm extends Component {
+  renderInput = (formProps) => {
+    const className = `field ${formProps.meta.touched && formProps.meta.error ? "error" : ""}`;
+    return (
+      <div className={className}>
+        <label>{formProps.label}</label>
+        <input
+          type="text"
+          placeholder={formProps.placeholder}
+          autoComplete="off"
+          {...formProps.input}
+        />
+        {this.renderError(formProps.meta)}
+      </div>
+    )
+  }
+
+  renderError = (meta) => {
+    if(meta.touched && meta.error) {
+      return (
+        <div className="ui error message" style={{display: "block", fontSize: "12px"}}>
+          <div className="header">{meta.error}</div>
+        </div>
+      )
+    }else {
+      return null
+    }
+  }
+
+  onSubmitHandler = (formValues) => {
+    this.props.onSubmit(formValues, this.props.userId);
+  }
+
+  render() {
+    if(this.props.userId) {
+      return (
+        <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmitHandler)}>
+          <Field name="title" component={this.renderInput} placeholder="Title..." label="Enter Title:"/>
+          <Field name="description" component={this.renderInput} placeholder="Description" label="Enter Description:"/>
+          <button className="ui button primary">Submit</button>
+        </form>
+      );
+    } else {
+      return (
+        <h2>You must be logged-in in order to create streams</h2>
+      )
+    }
+  }
+}
+
+const validateForm = (formValues) => {
+  const errors = {};
+
+  if(!formValues.title) {
+    errors.title = "You must enter a title"
+  }
+
+  if(!formValues.description) {
+    errors.description = "You must enter a description"
+  }
+
+  return errors;
+}
+
+const mapStateToProps = (state) => {
+  return {
+    userId: state.auth.userId
+  }
+}
+
+const form = reduxForm({
+  form: "StreamForm",
+  validate: validateForm
+})(StreamForm);
+
+export default connect(mapStateToProps)(form)
+
