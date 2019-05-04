0eef727694874fe1d781f73e1e2a5def246e09f6
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-04 10:10:04 -0400

    El StreamEdit reutiliza los valores del stream a editar como valores iniciales del formulario de edición

--- src/componentes/streams/StreamEdit.js
+++ src/componentes/streams/StreamEdit.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import {connect} from "react-redux";
-import {fetchSingleStream} from "../../actions";
+import {fetchSingleStream, editStream} from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends Component {
   componentDidMount() {
     this.props.streamToEdit(this.props.match.params.id)
   }
 
+  onSubmitHandler = (formValues) => {
+    console.log(formValues)
+  }
+
   render() {
-    console.log(this.props.stream)
+    if(!this.props.streamToEdit) {
+      return <div>Loading...</div>
+    }
     return (
       <div>
-        Stream Edit
+        <h3>Edit Stream</h3>
+        <StreamForm
+          initialValues={this.props.stream}
+          onSubmit={this.onSubmitHandler}
+        />
       </div>
     );
   }
@@ -27,6 +38,9 @@ const mapDispatchToProps = (dispatch) => {
   return {
     streamToEdit: (id) => {
       dispatch(fetchSingleStream(id))
+    },
+    editStream: () => {
+      dispatch(editStream())
     }
   }
 }
