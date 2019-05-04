2bd573958b015f71851a974edfceae1fe6839c05
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-04 08:51:24 -0400

    Se refactorizó StreamEdit a componente de clase para incluir redux

--- src/componentes/streams/StreamEdit.js
+++ src/componentes/streams/StreamEdit.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {connect} from "react-redux";
+import {fetchSingleStream} from "../../actions";
 
-const StreamEdit = () => {
-  return (
-    <div>
-      Stream Edit
-    </div>
-  );
+class StreamEdit extends Component {
+  componentDidMount() {
+    this.props.streamToEdit(this.props.match.params.id)
+  }
+
+  render() {
+    console.log(this.props.stream)
+    return (
+      <div>
+        Stream Edit
+      </div>
+    );
+  }
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    streamToEdit: (id) => {
+      dispatch(fetchSingleStream(id))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(StreamEdit);
