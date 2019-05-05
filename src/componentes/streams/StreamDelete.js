23f104e264629a5d7f7f0a2a5a1b135c635d5538
J3duardo <jesus_69eduardo@hotmail.com>
2019-05-05 18:07:07 -0400

    Se refactorizó el modal para utilizar props en lugar de contenido preestablecido

--- src/componentes/streams/StreamDelete.js
+++ src/componentes/streams/StreamDelete.js
@@ -2,10 +2,20 @@ import React from 'react';
 import Modal from "../Modal";
 
 const StreamDelete = () => {
+  const renderActions = (
+    <React.Fragment>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  )
   return (
     <div>
       Stream Delete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="re you sure you want to delete this stream?"
+        actions={renderActions}
+      />
     </div>
   );
 }
